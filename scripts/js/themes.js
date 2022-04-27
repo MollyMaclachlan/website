b57bd93838f09c999e47fcfb97c2ae8334c5f239
Murdo B. Maclachlan <murdomaclachlan@noreply.codeberg.org>
2022-04-27 22:40:47 +0100

    Added a cookie acceptance/declination banner.

--- scripts/js/themes.js
+++ scripts/js/themes.js
@@ -6,8 +6,24 @@ try {
     var prefix = "";
 }
 
+const COOKIE_HTML =
+    `<div id='cookie-popup' style='display:none;'>
+        <p>
+            This website may store cookies for the purposes of changing the font or theme, and for disabling this popup. Cookies are stored for 24 hours, and no other data is collected. <a href="legal">See more</a>.
+
+            If you decline, this popup will, by necessity continue to appear each time you load a page on this site.
+            <div id="cookie-button-container">
+                <button id="cookie-accept" class="nav-button" type="button" onclick="accept_cookie()">Accept</button><button id="cookie-decline" class="nav-button" type="button" onclick="decline_cookie()">Decline</button>
+            </div>
+        </p>
+    </div>`;
 const DAY_SECONDS = 86400000
 
+var body = document.getElementById('body-container');
+body.insertAdjacentHTML("afterbegin", COOKIE_HTML);
+
+var cookie = fetch_cookie("cookie");
+var cookie_popup = document.getElementById("cookie-popup");
 var font = fetch_cookie("font");
 var font_sheet = document.getElementById("font");
 var theme = fetch_cookie("theme");
@@ -18,6 +34,9 @@ for (var i of [["font", font, font_sheet], ["theme", theme, theme_sheet]]) {
     if (i[1] != null) {
         i[2].href = parse(prefix + "../static/css/themes/%v_%v.css", [i[0], i[1]]);
     }
+}
+if (cookie == null) {
+    cookie_popup.style.display = "block";
 }
 
 // Swap the theme (dark/light) based on the value currently held in "theme" variable
@@ -33,7 +52,6 @@ function change_theme() {
     if (pronouns_img != null) {
         pronouns_img.src = parse(prefix + "../static/img/banners/pronouns_%v_theme.webp", [theme]);
     }
-
     create_cookie("theme", theme);
 }
 
@@ -47,17 +65,32 @@ function change_font() {
         font = "serif";
     }
     font_sheet.href = parse(prefix + "../static/css/themes/font_%v.css", [font]);
-
     create_cookie("font", font);
+}
+
+// Create a cookie to register cookies as accepted, then fade out popup
+function accept_cookie() {
+    cookie = "accepted";
+    create_cookie("cookie", cookie);
+    fade(cookie_popup);
+}
+
+// Fade out popup, do nothing else
+function decline_cookie() {
+    fade(cookie_popup);
 }
 
 // COOKIE HANDLING
 
 function create_cookie(name, value) {
-    // Set the cookie to expire in one day
-    let expiry_date = new Date();
-    expiry_date.setTime(expiry_date.getTime() + DAY_SECONDS);
-    set_cookie(name, value, expiry_date);
+    // Only create cookies if they've been accepted
+    if (cookie == "accepted") {
+        // Set the cookie to expire in one day
+        let expiry_date = new Date();
+        expiry_date.setTime(expiry_date.getTime() + DAY_SECONDS);
+        set_cookie(name, value, expiry_date);
+        console.log("yeet");
+    }
 }
 
 // Fetch a cookie and parse information
@@ -90,4 +123,19 @@ function set_cookie(name, value, expiry_date) {
 function parse(str, targets) {
     var i = 0;
     return str.replace(/%v/g, () => targets[i++]);
+}
+
+// Taken from this StackOverflow answer:
+// https://stackoverflow.com/questions/6121203/how-to-do-fade-in-and-fade-out-with-javascript-and-css
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.01){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
 }
