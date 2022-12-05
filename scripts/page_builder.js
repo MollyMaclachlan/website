dd672af7585ab5f41f785b7a7534cff28c8485d1
Murdo B. Maclachlan <murdomaclachlan@duck.com>
2022-12-05 21:04:31 +0000

    Complete website redesign using Bulma.

---
+++ scripts/page_builder.js
@@ -0,0 +1,112 @@
+// Fetch the page's prefix for compatibility between pages in the top-level folder & those in subfolders
+try {
+    var prefix = document.getElementsByName('prefix')[0].content;
+} catch (TypeError) {
+    var prefix = "";
+}
+
+/**
+ * Inserts the universal elements of the website into the page.
+ */
+function build_page() {
+
+    // Header and footer
+    insert_structure("header");
+    insert_structure("footer");
+}
+
+/**
+ * Inserts a script element into the page's list of scripts.
+ *
+ * @param source The path to the script
+ */
+function insert_script(source) {
+    let script = document.createElement('script');
+    script.src = source;
+    document.getElementById('scripts').appendChild(script);
+}
+
+/**
+ * Fetches and inserts an HTML structure before the end of a unique element
+ *
+ * @param structure The name of the structure
+ */
+function insert_structure(structure) {
+    console.log(`${prefix}../scripts/structures/${structure}.html`);
+    let parser = new DOMParser();
+    fetch(`${prefix}../scripts/structures/${structure}.html`)
+    .then(response=> response.text())
+    .then(text=> document.getElementById(`container-${structure}`).insertAdjacentHTML(
+        "beforeend",
+        modify_structure(parser.parseFromString(text, 'text/html'), structure)
+    ));
+}
+
+/**
+ * Modifies a given footer structure to ensure all links are compatible with the page's
+ * position within the file structure.
+ *
+ * @param structure The structure to modify
+ *
+ * @return The modified structure
+ */
+function modify_footer(structure) {
+    structure = modify_links(structure);
+    return structure;
+}
+
+/**
+ * Modifies a given header structure to ensure all links are compatible with the page's
+ * position within the file structure.
+ *
+ * @param structure The structure to modify
+ *
+ * @return The modified structure
+ */
+function modify_header(structure) {
+    structure = modify_links(structure);
+    let logo = structure.getElementById('logo');
+    logo.src = `${prefix}${logo.dataset['src']}`;
+    return structure;
+}
+
+/**
+ * Modifies all 'a' elements in a given structure.
+ * 
+ * @param structure The structure to modify the 'a' elements of
+ * 
+ * @return The modified structure
+ */
+function modify_links(structure) {
+    for (element of structure.getElementsByTagName('a')) {
+        let target = element.dataset['target'];
+        if (target != null) {
+            element.href = `${prefix}${target}`;
+        }
+    }
+    return structure;
+}
+
+/**
+ * Modifies a given HTML structure to ensure all links are compatible with the page's
+ * position within the file structure.
+ *
+ * @param structure The HTML structure to modify
+ * @param name      The name of the structure; either 'header' or 'footer'
+ *
+ * @return The header or footer element from within the modified structure
+ */
+function modify_structure(structure, name) {
+    switch (name) {
+        case 'footer':
+            structure = modify_footer(structure);
+            break;
+        case 'header':
+            structure = modify_header(structure);
+            break;
+        default:
+    }
+    return structure.getElementsByTagName('body')[0].innerHTML;
+}
+
+build_page();
---
+++ scripts/tabulator.js
@@ -0,0 +1,64 @@
+var tabs_widget = document.getElementById('tabset');
+var tabs_links = tabs_widget.children[0].children;
+var tabset = tabs_widget.dataset['tabset'].split(' ');
+
+/**
+ * Switches the active tab in the page's tabset.
+ * 
+ * @param trigger The element that triggered the switch
+ */
+function switch_tab(trigger) {
+    switch_active(trigger);
+    switch_shown(trigger);
+}
+
+/**
+ * Switches the active tab title to the one that triggered the switch.
+ * 
+ * @param trigger The element that triggered the switch
+ */
+function switch_active(trigger) {
+    for (element of tabs_links) {
+        class_toggler(
+            'is-active',
+            (element == trigger),
+            element
+        );
+    }
+}
+
+/**
+ * Switches the shown tab to the one targeted by the one that triggered the
+ * switch.
+ * 
+ * @param trigger The element that triggered the switch
+ */
+function switch_shown(trigger) {
+    for (element of tabset) {
+        let current_section = document.getElementById(element);
+        class_toggler(
+            'hidden',
+            (element != trigger.dataset['target']),
+            current_section
+        );
+    }
+}
+
+/**
+ * Toggles the presence of a class in a given target element, based on the given
+ * truth value of a boolean condition.
+ * 
+ * @param class_name The name of the class to toggle
+ * @param condition  The boolean truth value of the condition
+ * @param target     The target element
+ */
+function class_toggler(class_name, condition, target) {
+    let has_class = target.classList.contains(class_name);
+    if (condition) {
+        if (!has_class) {
+            target.classList.add(class_name);
+        }
+    } else if (has_class) {
+        target.classList.remove(class_name);
+    }
+}
